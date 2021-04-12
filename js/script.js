220e5372dfc7eeae520173a6925ae300ac88e193
DociIva <doci.iva@hotmail.com>
2021-04-12 17:56:30 +0200

    prima

--- js/script.js
+++ js/script.js
@@ -1,2 +1,13 @@
-console.log('Ciao mondo');
-console.log('jQuery', jQuery);
+//console.log('Ciao mondo');
+//console.log('jQuery', jQuery);
+$(document).ready(function(){
+    var numeri = [ 17 , 36 , 44 , 5 , 3];
+
+    for( var i = 0; i < 57; i++) {
+        
+        alert('Ricorda questi numeri ' + numeri);
+    
+
+    }
+  
+}); 
